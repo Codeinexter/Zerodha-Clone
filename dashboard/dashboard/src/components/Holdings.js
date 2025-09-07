15e65170704ae005fb13cfdc18a9b503c53f9047
Roshaid Atique <roshaid2004@gmail.com>
2025-09-08 01:31:13 +0530

    charts and visualizations

--- dashboard/dashboard/src/components/Holdings.js
+++ dashboard/dashboard/src/components/Holdings.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { VerticalGraph } from "./VerticalGraph";
 //import {holdings} from "../data/data";
 const Holdings = () => {
 
@@ -11,6 +12,19 @@ const Holdings = () => {
       setAllHoldings(res.data);
     })
   }, []);
+
+  const labels = allHoldings.map((subarray) => subarray["name"]);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Stock Price",
+        data: allHoldings.map((stock) => stock.price),
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      }
+    ],
+  };
 
   return (
     <>
@@ -69,6 +83,7 @@ const Holdings = () => {
           <p>P&L</p>
         </div>
       </div>
+      <VerticalGraph data={data}/>
     </>
   );
 };
